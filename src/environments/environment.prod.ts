69a48e6e4866c8a732f5ae9f0b515f9c1ba0293e
kashifmzai <asadu125@gmail.com>
2024-06-25 00:09:11 -0400

    add build bundle

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB-iQAQygwbn8V2XovPlOQHlIc_I5gWgYo",
+    authDomain: "careercloudhumber.firebaseapp.com",
+    projectId: "careercloudhumber",
+    storageBucket: "careercloudhumber.appspot.com",
+    messagingSenderId: "16468528619",
+    appId: "1:16468528619:web:680dc401e4004090245dae",
+  },
+};
